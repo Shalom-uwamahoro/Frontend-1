51b6d3252c5ae47f604098921f05942dc44b661e
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-02-07 16:39:49 +0300

    add one file

---
+++ Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var fullName = "Shalom Uwamahoro"
+println(fullName)
+    var age = 23
+    println(age)
+    var phoneNumber = +250783430922
+    println(phoneNumber)
+    var weightInKg= 58
+    println(weightInKg)
+    var citizen = "true"
+    val nonCitizen = "false"
+    println(citizen)
+
+
+}
